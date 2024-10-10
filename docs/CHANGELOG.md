69e5de49d7ddf7cc7a74aca84c2551336f69ce28
RzR <108324929+I-RzR-I@users.noreply.github.com>
2024-10-10 22:22:18 +0300

    Update reference package version, fixing CVE

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -1,2 +1,5 @@
 ### **v.1.0.1.0810** 
 -> Update lib extension version. <br/>
+
+### **v1.0.2.8005** 
+-> Update reference package version, fixing CVE (`CVE-2024-43485`).
--- src/shared/GeneralAssemblyInfo.cs
+++ src/shared/GeneralAssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Resources;
 
 [assembly: AssemblyCompany("RzR ®")]
 [assembly: AssemblyProduct("e-GOV MD MPassHelperDotNet")]
-[assembly: AssemblyCopyright("Copyright © 2022-2023 RzR All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2022-2024 RzR All rights reserved.")]
 [assembly: AssemblyTrademark("® RzR™")]
 [assembly:
     AssemblyDescription(
@@ -43,6 +43,6 @@ using System.Resources;
 
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
 
-[assembly: AssemblyVersion("1.0.1.0810")]
-[assembly: AssemblyFileVersion("1.0.1.0810")]
-[assembly: AssemblyInformationalVersion("1.0.1.*")]
+[assembly: AssemblyVersion("1.0.2.8005")]
+[assembly: AssemblyFileVersion("1.0.2.8005")]
+[assembly: AssemblyInformationalVersion("1.0.2.8005")]
